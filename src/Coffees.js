7c45315bf2304d2f09fc6adf2abdb5ba18f3666b
Piotr Lewandowski <piotrl@Piotrs-MacBook-Pro.local>
2022-02-14 11:47:28 -0500

    added useState('hot')

--- src/Coffees.js
+++ src/Coffees.js
@@ -2,17 +2,20 @@ import React, { useState, useEffect } from "react";
 
 function Coffees() {
   const [coffeeList, setCoffeeList] = useState();
+  const [temp, setTemp] = useState('hot');
   useEffect(() => {
-    fetch("https://api.sampleapis.com/coffee/hot")
+    fetch(`https://api.sampleapis.com/coffee/${temp}`)
       .then((response) => response.json())
       .then((data) => setCoffeeList(data))
       .catch((err) => console.error(err));
-  }, []);
+  }, [temp]);
   return (
     //use parentheses here, not brackets
     <section className="coffee-container" id="coffees">
-      {/*need one containing element, don't need it to be a react fragment*/}
+      {/*need one containing element, don't need it to be a react fragment (<></> if we dont want any HTML to render) */}
       <h2>Coffee Recipes</h2>
+      <button onClick={()=>setTemp('hot')}>Hot</button> 
+      <button onClick={()=>setTemp('iced')}>Iced</button>
       {!coffeeList ? (
         <h3>Loading...</h3>
       ) : (
